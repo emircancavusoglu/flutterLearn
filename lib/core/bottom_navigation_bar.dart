8ee29182d5ba91d7a64e306d77f24b34284372e6
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-08-25 21:55:56 +0300

    add enums

--- lib/core/bottom_navigation_bar.dart
+++ lib/core/bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterprojemm/core/life_cycle.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({Key? key}) : super(key: key);
@@ -10,10 +11,18 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigationState extends State<BottomNavigation> {
   @override
   Widget build(BuildContext context) {
-   return BottomNavigationBar(items: const <BottomNavigationBarItem>[
+   return BottomNavigationBar(onTap: (value){
+     if(value == Labels.newPage.index){
+       Navigator.push(context, MaterialPageRoute(builder: (context) => LifeCycle(name: "EMİRCAN"),));
+     }
+   }, items: const <BottomNavigationBarItem>[
      BottomNavigationBarItem(icon: Icon(Icons.account_balance),label: "Home"),
      BottomNavigationBarItem(icon: Icon(Icons.adb),label: "New Page"),
    ]
    );
   }
+}
+enum Labels{
+ home,
+ newPage
 }
