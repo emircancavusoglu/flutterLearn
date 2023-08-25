ac85d029db03e7c73e334321bcce08c740149e75
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-08-25 21:40:38 +0300

    create main page

---
+++ lib/core/main_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutterprojemm/core/page_view_learn.dart';
+import 'package:flutterprojemm/core/text_field_learn.dart';
+import 'package:flutterprojemm/main.dart';
+
+import '../const/string_consts.dart';
+import '../product/customButton.dart';
+import 'life_cycle.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  // CustomButton custom = const CustomButton();
+  int counter = 0;
+  int _counterCustom = 0;
+  void update(bool isIncrement){
+    if(isIncrement){
+      setState(() {
+        print("basıldı");
+        ++counter;
+      });
+    }
+    else{
+      setState(() {
+        print("basıldı");
+        --counter;
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [Padding(
+          padding: EdgeInsets.only(right: 16.0),
+          child: Icon(Icons.add_a_photo),
+        )],
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            update(true);
+          }, child: const Icon(Icons.add)),
+          ElevatedButton(onPressed: (){
+            update(false);
+          }, child: const Icon(Icons.remove)),
+          const CustomButtton(),
+          Center(
+            child: Text(counter.toString()),
+          ),
+          const SizedBox(height: 20,),
+          ElevatedButton(onPressed: (){
+            navigateToPageView(context);
+          }, child: const Text("Yeni Sayfaya Geç")),
+          ElevatedButton(onPressed: (){
+            navigateToLifeCycle(context);
+          }, child: Text(StringConst.lifeCyle)),
+          ElevatedButton(onPressed: (){
+            navigateToTextField(context);
+          }, child: Text(StringConst.textField))
+        ],
+      ),
+
+    );
+  }
+
+  void navigateToPageView(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  PageViewLearn(message: '',),));
+  }
+
+  void navigateToLifeCycle(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  LifeCycle(name: 'Emircan',),));
+  }
+
+  void navigateToTextField(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TextFieldLearn(),));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import'package:flutter/material.dart';
-import 'package:flutterprojemm/const/string_consts.dart';
-import 'package:flutterprojemm/core/life_cycle.dart';
-import 'package:flutterprojemm/core/page_view_learn.dart';
-import 'package:flutterprojemm/product/customButton.dart';
-import 'package:flutterprojemm/core/text_field_learn.dart';
+import 'core/main_page.dart';
 
 void main() => runApp( MyApp());
 
@@ -18,77 +14,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatefulWidget {
-  MainScreen({Key? key}) : super(key: key);
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  // CustomButton custom = const CustomButton();
-  int counter = 0;
-  int _counterCustom = 0;
-  void update(bool isIncrement){
-    if(isIncrement){
-      setState(() {
-        print("basıldı");
-        ++counter;
-      });
-    }
-    else{
-      setState(() {
-        print("basıldı");
-        --counter;
-      });
-    }
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: const [Padding(
-          padding: EdgeInsets.only(right: 16.0),
-          child: Icon(Icons.add_a_photo),
-        )],
-      ),
-      body: Column(
-         children: [
-           ElevatedButton(onPressed: (){
-             update(true);
-           }, child: const Icon(Icons.add)),
-           ElevatedButton(onPressed: (){
-             update(false);
-           }, child: const Icon(Icons.remove)),
-           const CustomButtton(),
-          Center(
-            child: Text(counter.toString()),
-          ),
-          const SizedBox(height: 20,),
-          ElevatedButton(onPressed: (){
-            navigateToPageView(context);
-          }, child: const Text("Yeni Sayfaya Geç")),
-           ElevatedButton(onPressed: (){
-             navigateToLifeCycle(context);
-           }, child: Text(StringConst.lifeCyle)),
-           ElevatedButton(onPressed: (){
-             navigateToTextField(context);
-           }, child: Text(StringConst.textField))
-        ],
-      ),
-
-    );
-  }
-
-  void navigateToPageView(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  PageViewLearn(message: '',),));
-  }
-
-  void navigateToLifeCycle(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) =>  LifeCycle(name: 'Emircan',),));
-  }
-
-  void navigateToTextField(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TextFieldLearn(),));
-  }
-}
