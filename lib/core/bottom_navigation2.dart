972bca5e9115280953268ffec43130f2105a36d8
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-09-06 01:28:04 +0300

    add colors

--- lib/core/bottom_navigation2.dart
+++ lib/core/bottom_navigation2.dart
@@ -9,17 +9,32 @@ class BottomExample2 extends StatefulWidget {
 }
 
 class _BottomExample2State extends State<BottomExample2> {
+
+  void changeColor(Color color){
+    backgroundColor = color;
+  }
+  Color backgroundColor = Colors.transparent;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (value){
-
+          if(value == 0){
+            setState(() {
+              changeColor(Colors.teal);
+            });
+          }
+          else if(value == 1){
+            setState(() {
+              changeColor(Colors.tealAccent);
+            });
+          }
         },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.change_circle_rounded), label: "Change"),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
+          BottomNavigationBarItem(icon: Icon(Icons.change_circle_rounded), label: "Add"),
         ],
       ),
     );
