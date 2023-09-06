e37ce18f1c6de96c7d4f0abd3ae1747f3510b927
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-09-07 01:19:03 +0300

    add new page and sample app

--- lib/core/example_2.dart
+++ lib/core/example_2.dart
@@ -9,11 +9,47 @@ class MySample extends StatefulWidget {
 }
 
 class _MySampleState extends State<MySample> {
+  late final List<CollectionModels> _items;
+  @override
+  void initState() {
+    super.initState();
+    _items = [
+     CollectionModels(imagePath: 'assets/book.png', title: "title", price: 3.4)
+    ];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("sample try"),
+      body: ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: SizedBox(
+              height: 300,
+              child: Expanded(
+                child: Column(
+                  children: [
+                    Image.asset(_items[index].imagePath),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(_items[index].title),
+                        Text('${_items[index].price}')
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+      },),
     );
   }
+}
+class CollectionModels{
+  String imagePath;
+  String title;
+  double price;
+
+  CollectionModels({required this.imagePath, required this.title, required this.price});
 }
