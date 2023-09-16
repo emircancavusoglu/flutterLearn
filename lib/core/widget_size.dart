7e3243c2c7484710614301af5fc042a39323b39c
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2023-09-17 01:38:09 +0300

    add extension and enum

---
+++ lib/core/widget_size.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutterprojemm/theme/light_theme.dart';
+
+class WidgetSize extends StatefulWidget {
+  const WidgetSize({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetSize> createState() => _WidgetSizeState();
+}
+
+class _WidgetSizeState extends State<WidgetSize> {
+  LightTheme lightTheme = LightTheme();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: lightTheme.theme.appBarTheme.backgroundColor,
+      ),
+      body: Card(
+        child: Container(
+          height: WidgetSizes.normalCardHeight.value(),
+          color: Colors.indigo,
+        ),
+      ),
+    );
+  }
+}
+
+enum WidgetSizes{
+  normalCardHeight, circleProfileWidth
+}
+
+extension WidgetSizesExtension on WidgetSizes{
+  double value(){
+    switch(this){
+      case WidgetSizes.normalCardHeight:
+        return 25;
+        break;
+      case WidgetSizes.circleProfileWidth:
+        return 35;
+        break;
+    }
+
+  }
+}
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 class LightTheme{
-
   ThemeData theme = ThemeData(
+    appBarTheme: const AppBarTheme(backgroundColor: Colors.indigo,centerTitle: false),
     floatingActionButtonTheme: const FloatingActionButtonThemeData(backgroundColor: Colors.indigo),
+    textTheme: ThemeData.light().textTheme.copyWith(
+      titleMedium: const TextStyle(
+        fontSize: 24,
+        color: Colors.black54,
+      ),
+    ),
   );
 
 }
